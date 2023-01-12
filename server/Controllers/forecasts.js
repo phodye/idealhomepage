2f3bbe5be45a3c194e55a9053dc0662ba35e9934
Andrew Dye <phodye@gmail.com>
2023-01-12 18:19:04 -0500

    Added option to update forecast

--- server/Controllers/forecasts.js
+++ server/Controllers/forecasts.js
@@ -3,28 +3,39 @@ const axios = require('axios')
 require('dotenv').config()
 
 const addForecast = (request, response) => {
-  console.log('city and state in controller', request.body)
 
   let cityName = request.body.city
   let stateCode = request.body.state
   let countryCode = request.body.country
   let existQuery = `SELECT * FROM forecasts WHERE CITYNAME = $1`
   let addQuery = `INSERT INTO forecasts (cityname, statecode, countrycode, currenttemp, hightemp, lowtemp, descriptor, weathericon) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+  let updateQuery = `UPDATE forecasts SET currenttemp = $1, hightemp = $2, lowtemp = $3, descriptor = $4, weathericon = $5 WHERE cityname = $6`
 
   pool
     .query(existQuery, [cityName])
     .then((res) => {
-      console.log(res.rows)
       if (res.rows.length === 0) {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${process.env.appid}`)
         .then((data) => {
-          console.log(data.data)
           let forecast = data.data
           pool
             .query(addQuery, [cityName, stateCode, countryCode, forecast.main.temp, forecast.main.temp_max, forecast.main.temp_min, forecast.weather[0].main, forecast.weather[0].icon])
               .then(response.send())
               .catch((err) => {
                 console.log('error adding forecast to db', err)
+                response.status(500)
+              })
+        })
+        .catch((err) => console.log(err))
+      } else {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${process.env.appid}`)
+        .then((data) => {
+          let forecast = data.data
+          pool
+            .query(updateQuery, [forecast.main.temp, forecast.main.temp_max, forecast.main.temp_min, forecast.weather[0].main, forecast.weather[0].icon, cityName])
+              .then(response.send())
+              .catch((err) => {
+                console.log('error updating forecast in db', err)
                 response.status(500)
               })
         })
