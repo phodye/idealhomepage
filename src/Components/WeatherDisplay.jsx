00050e311e62428e647054310560178b4f9bb270
Andrew Dye <phodye@gmail.com>
2023-01-13 09:19:10 -0500

    Added basic forecast card

--- src/Components/WeatherDisplay.jsx
+++ src/Components/WeatherDisplay.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const WeatherDisplay = () => {
+const WeatherDisplay = ({city}) => {
+  console.log(city)
+  const convertToFarhrenheit = (temp) => {
+    return Math.floor(9/5 * (temp - 273) + 32)
+  }
 
   return (
     <>
-      <h1>
-        Weather Display
-      </h1>
+      <div className="forecast">
+        <p>{city.cityname}, {city.statecode}</p>
+        <p>{city.descriptor}</p>
+        <p>Current Temperature: {convertToFarhrenheit(city.currenttemp)}</p>
+        <p>High Temperature: {convertToFarhrenheit(city.hightemp)}</p>
+        <p>Low Temperature: {convertToFarhrenheit(city.lowtemp)}</p>
+        <img src={`http://openweathermap.org/img/w/${city.weathericon}.png`}></img>
+      </div>
     </>
   );
 }
